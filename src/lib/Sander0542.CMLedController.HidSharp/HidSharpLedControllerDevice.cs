6ced34b11bf9af8bfc8a2f5a6636f8c94e6bdbad
Sander Jochems <sanderjochems@hotmail.nl>
2021-08-11 13:33:20 +0200

    Create HidSharpLedControllerDevice

---
+++ src/lib/Sander0542.CMLedController.HidSharp/HidSharpLedControllerDevice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using HidSharp;
+using Sander0542.CMLedController.Abstractions;
+
+namespace Sander0542.CMLedController.HidSharp
+{
+    public class HidSharpLedControllerDevice : LedControllerDevice<HidDevice>
+    {
+        private readonly HidStream _stream;
+
+        public HidSharpLedControllerDevice(HidDevice device) : base(device)
+        {
+        }
+
+        protected override async Task WriteAsync(byte[] data, CancellationToken token = default)
+        {
+        }
+    }
+}
