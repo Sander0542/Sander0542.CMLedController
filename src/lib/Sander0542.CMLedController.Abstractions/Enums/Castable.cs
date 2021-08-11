08deeb72eb72913fc41ba00375ecbabd02e7a1cb
Sander Jochems <sanderjochems@hotmail.nl>
2021-08-11 18:28:48 +0200

    Create Castable.cs

---
+++ src/lib/Sander0542.CMLedController.Abstractions/Enums/Castable.cs
@@ -0,0 +1,14 @@
+﻿namespace Sander0542.CMLedController.Abstractions.Enums
+{
+    public abstract class Castable<T>
+    {
+        private readonly T _value;
+
+        protected Castable(T value)
+        {
+            _value = value;
+        }
+        
+        public static implicit operator T(Castable<T> mode) => mode._value;
+    }
+}
