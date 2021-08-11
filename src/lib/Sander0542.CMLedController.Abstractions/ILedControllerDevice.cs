c24876e5b2b438da50e7b52078bca9c888abc4b2
Sander Jochems <sanderjochems@hotmail.nl>
2021-08-11 18:28:29 +0200

    Update ILedControllerDevice.cs

--- src/lib/Sander0542.CMLedController.Abstractions/ILedControllerDevice.cs
+++ src/lib/Sander0542.CMLedController.Abstractions/ILedControllerDevice.cs
@@ -1,11 +1,35 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Threading;
 using System.Threading.Tasks;
+using Sander0542.CMLedController.Abstractions.Enums;
 
 namespace Sander0542.CMLedController.Abstractions
 {
-    public interface ILedControllerDevice
+    public interface ILedControllerDevice : IDisposable
     {
-        public Task SendRequestAsync(IRequest request, CancellationToken token = default);
+        string GetDeviceName();
+
+        string GetSerial();
+
+        string GetLocation();
+
+        Mode GetMode();
+
+        byte GetSpeed();
+
+        byte GetBrightness();
+
+        Color GetModeColor(int colorNumber);
+
+        Color GetPortColor(int portNumber);
+
+        Task SetModeAsync(Mode mode, byte speed, byte brightness, Color color1, Color color2, CancellationToken token = default);
+
+        Task SetLedsDirectAsync(Color color1, Color color2, Color color3, Color color4, CancellationToken token = default);
+
+        Task ReadCurrentModeAsync(CancellationToken token = default);
+
+        Task ReadModeConfigAsync(Mode mode, CancellationToken token = default);
     }
 }
