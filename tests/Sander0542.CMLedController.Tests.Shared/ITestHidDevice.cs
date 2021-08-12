86aba28392596bc6667283115771be69584bc9ab
Sander Jochems <sanderjochems@hotmail.nl>
2021-08-12 18:22:37 +0200

    Create Test device and controller

---
+++ tests/Sander0542.CMLedController.Tests.Shared/ITestHidDevice.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Sander0542.CMLedController.Tests.Shared
+{
+    public interface ITestHidDevice : IDisposable
+    {
+        public void Write(byte[] data);
+
+        public byte[] WriteAndRead(byte[] data);
+    }
+}
---
+++ tests/Sander0542.CMLedController.Tests.Shared/TestLedControllerDevice.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Sander0542.CMLedController.Abstractions;
+
+namespace Sander0542.CMLedController.Tests.Shared
+{
+    public class TestLedControllerDevice : LedControllerDevice<ITestHidDevice>
+    {
+        public TestLedControllerDevice(ITestHidDevice device) : base(device)
+        {
+        }
+
+        protected override async Task WriteAsync(byte[] data, CancellationToken token = default)
+        {
+            Device.Write(data);
+        }
+
+        protected override async Task<byte[]> WriteAndReadAsync(byte[] data, CancellationToken token = default)
+        {
+            return Device.WriteAndRead(data);
+        }
+
+        public override void Dispose()
+        {
+            Device.Dispose();
+        }
+    }
+}
