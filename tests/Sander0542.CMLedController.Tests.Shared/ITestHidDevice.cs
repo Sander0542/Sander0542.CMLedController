fdd6fac28d7fbd167d100d06121fbd024a31dbf8
Sander Jochems <sanderjochems@hotmail.nl>
2021-08-13 01:04:11 +0200

    Update ITestHidDevice

--- tests/Sander0542.CMLedController.Tests.Shared/ITestHidDevice.cs
+++ tests/Sander0542.CMLedController.Tests.Shared/ITestHidDevice.cs
@@ -4,8 +4,6 @@ namespace Sander0542.CMLedController.Tests.Shared
 {
     public interface ITestHidDevice : IDisposable
     {
-        public void Write(byte[] data);
-
         public byte[] WriteAndRead(byte[] data);
     }
 }
