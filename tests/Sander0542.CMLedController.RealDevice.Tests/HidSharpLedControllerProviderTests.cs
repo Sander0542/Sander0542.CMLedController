d6d6c2846a0f2cba881fb158bdc1e0e4b6cb4a4d
Sander Jochems <sanderjochems@hotmail.nl>
2021-08-13 11:35:32 +0200

    Create HidSharpLedControllerProviderTests

---
+++ tests/Sander0542.CMLedController.RealDevice.Tests/HidSharpLedControllerProviderTests.cs
@@ -0,0 +1,12 @@
+﻿using Sander0542.CMLedController.HidSharp;
+using Sander0542.CMLedController.Tests.Shared.Fixtures;
+
+namespace Sander0542.CMLedController.RealDevice.Tests
+{
+    public class HidSharpLedControllerProviderTests : LedControllerProviderTests<HidSharpLedControllerProvider>
+    {
+        public HidSharpLedControllerProviderTests(LedControllerProviderFixture<HidSharpLedControllerProvider> providerFixture) : base(providerFixture)
+        {
+        }
+    }
+}
