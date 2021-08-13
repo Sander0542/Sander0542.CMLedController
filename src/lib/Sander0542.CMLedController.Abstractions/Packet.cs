641ebaaa85cae0ac912218e32f2bdae18e8a349b
Sander Jochems <sanderjochems@hotmail.nl>
2021-08-13 16:58:17 +0200

    Create Packet class with properties for all PacketOffsets

---
+++ src/lib/Sander0542.CMLedController.Abstractions/Packet.cs
@@ -0,0 +1,106 @@
+﻿using System.Drawing;
+using Sander0542.CMLedController.Abstractions.Enums;
+using Sander0542.CMLedController.Abstractions.Extensions;
+
+namespace Sander0542.CMLedController.Abstractions
+{
+    public class Packet
+    {
+        private readonly byte[] _data;
+
+        public Packet()
+        {
+            _data = new byte[Constants.PacketSize];
+        }
+
+        public Packet(byte[] data)
+        {
+            _data = data;
+        }
+
+        public OpCode Operation
+        {
+            get => _data[PacketOffset.Op];
+            set => _data[PacketOffset.Op] = value;
+        }
+
+        public OpCodeType OperationType
+        {
+            get => _data[PacketOffset.Type];
+            set => _data[PacketOffset.Type] = value;
+        }
+
+        public byte Multilayer
+        {
+            get => _data[PacketOffset.Multilayer];
+            set => _data[PacketOffset.Multilayer] = value;
+        }
+
+        public Mode Mode
+        {
+            get => _data[PacketOffset.Mode];
+            set => _data[PacketOffset.Mode] = value;
+        }
+
+        public byte Speed
+        {
+            get => _data[PacketOffset.Speed];
+            set => _data[PacketOffset.Speed] = value;
+        }
+
+        public byte Brightness
+        {
+            get => _data[PacketOffset.Brightness];
+            set => _data[PacketOffset.Brightness] = value;
+        }
+
+        public Color Color1
+        {
+            get => _data.GetColor(PacketOffset.Color1);
+            set => _data.SetColor(PacketOffset.Color1, value);
+        }
+
+        public Color Color2
+        {
+            get => _data.GetColor(PacketOffset.Color2);
+            set => _data.SetColor(PacketOffset.Color2, value);
+        }
+
+        public Color MultipleColor1
+        {
+            get => _data.GetColor(PacketOffset.MultipleColor1);
+            set => _data.SetColor(PacketOffset.MultipleColor1, value);
+        }
+
+        public Color MultipleColor2
+        {
+            get => _data.GetColor(PacketOffset.MultipleColor2);
+            set => _data.SetColor(PacketOffset.MultipleColor2, value);
+        }
+
+        public Color MultipleColor3
+        {
+            get => _data.GetColor(PacketOffset.MultipleColor3);
+            set => _data.SetColor(PacketOffset.MultipleColor3, value);
+        }
+
+        public Color MultipleColor4
+        {
+            get => _data.GetColor(PacketOffset.MultipleColor4);
+            set => _data.SetColor(PacketOffset.MultipleColor4, value);
+        }
+
+        public int Length => _data.Length;
+
+        public byte Get(PacketOffset packetOffset) => _data[packetOffset];
+
+        public byte Get(int index) => _data[index];
+
+        public void Set(PacketOffset packetOffset, byte value) => _data[packetOffset] = value;
+
+        public void Set(int index, byte value) => _data[index] = value;
+
+        public static implicit operator byte[](Packet packet) => packet._data.PreparePacket();
+        public static implicit operator Packet(byte[] data) => new Packet(data.PrepareResponse());
+    }
+}
